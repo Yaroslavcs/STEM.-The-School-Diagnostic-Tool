3ce45195cc32c103898b98b3e0303598ed1ccae9
Yaroslavcs <sapsai.yaroslav@chnu.edu.ua>
2025-07-02 08:01:48 +0300

    Add files via upload
    1 частина Великого фіксу роботоздатність проєкту була зроблена версія 1.0

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProjectApi.Database.Data;
+using ProjectApi.Bussines_Logic.Services;
+using ProjectApi.Bussines_Logic.Helpers;
+using ProjectApi.Bussines_Logic.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using ProjectApi.Database.Repositories;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<ProjectApi.Bussines_Logic.Validators.ChangePasswordDtoValidator>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Реєстрація репозиторіїв
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRecommendationRepository, RecommendationRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Реєстрація сервісів
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IRecommendationService, RecommendationService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+var app = builder.Build();
+
+app.UseMiddleware<API.Middlewares.ErrorHandlingMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
